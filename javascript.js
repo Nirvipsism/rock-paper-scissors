98ea2b620a684d3fcfe27614a23a01b45c582b9b
Nirvipsism <184086439+Nirvipsism@users.noreply.github.com>
2024-12-24 11:33:36 +0100

    Build rng 0 to 2 and associate each number to rock, paper or scissor

---
+++ javascript.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!")
+
+function getComputerChoice {
+    let randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === O) {
+        return "Rock";
+    } else if (randomNum === 1) {
+        return "Paper";
+    } else { 
+        return "Scissors";
+    }
+}
